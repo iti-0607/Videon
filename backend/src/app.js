22959b1c9a628f8363f04f5eaeedc8a9b62c200c
iti-0607 <itiagrawal01@gmail.com>
2026-07-20 17:55:46 +0530

    setting up backend structure and db models

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,18 +4,23 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 
 import mongoose from "mongoose";
-
+import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors";
 
 const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
-
-app.get("/home", (req, res)=>{
-    return res.json({"hello":"world"})
-});
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended:true }))
 
 const start = async () => {
-    app.listen(8000, () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://videoDB:VIDEOdb%40123@cluster0.rihno00.mongodb.net/");
+
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
         console.log("Listening on port 8000");
     });
 

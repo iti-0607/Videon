3dcca493d66de83e63e8de6f463217daef6d009a
iti-0607 <itiagrawal01@gmail.com>
2026-07-20 21:07:04 +0530

    Authentication Routes set

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import { createServer } from "node:http";
 
@@ -8,6 +9,7 @@ import { Server } from "socket.io";
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
@@ -16,7 +18,8 @@ const io = connectToSocket(server);
 app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
-app.use(express.urlencoded({ limit: "40kb", extended:true }))
+app.use(express.urlencoded({ limit: "40kb", extended:true }));
+app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     const connectionDb = await mongoose.connect(process.env.MONGO_URI);
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,14 +1,14 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-
-const userSchema = new Schema(
+const userScheme = new Schema(
     {
         name: { type: String, required: true },
         username: { type: String, required: true, unique: true },
-        password: { type:  String, required: true },
+        password: { type: String, required: true },
         token: { type: String }
     }
 )
 
-const user = mongoose.model("User", userScheme);
+const User = mongoose.model("User", userScheme);
+
 export { User };
